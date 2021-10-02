b1f0d6856ace5fc492c782b88f837330e819efca
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-10-02 10:37:46 -0400

    added templates and routes

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+''' Route for login page'''
 @app.route('/')
 
 def index():
     return render_template("login.html")
+
+''' Route for register page'''
+
+@app.route('/register.html')
+
+def new_profile():
+    return render_template("register.html")
+
+# Route for profile page
+@app.route('/new')
+
+def profile():
+    return render_template("profile.html")
 
 
 

d0f9081cfa640236ddc2aa2c854cb14c57187e55
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-10-12 21:42:01 -0400

    adding routes to connecting profile to smpt server

--- mail.py
+++ mail.py
@@ -3,9 +3,11 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 
+
+# this is config file for email
 app.config['DEBUG'] = True
 app.config['TESTING'] = False
-app.config['MAIL_SERVER'] = 'smpt.gmail.com'
+app.config['MAIL_SERVER'] = 'smpt.gmail.com' #server i'm using gmail
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
@@ -16,3 +18,13 @@ app.config['MAIL_DEFAULT_SENDER'] = 'randolfh.cwd@gmail.com'
 app.config['MAIL_MAX_EMAILS'] = None
 app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['MAIL_ASCII_ATTACHEMENTS'] = False
+
+#mail application
+mail = Mail(app)
+
+#create route for cwd-profile page
+
+# @app.route('/profile.html')
+# def index():
+#     msg = Message('Hey There', send)
+

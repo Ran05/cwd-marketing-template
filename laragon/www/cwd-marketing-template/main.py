a1f08e8a29ba9eab9029014d044ab761c2c900bf
Ran05 <ferarizarandolfh.ifliba@gmail.com>
2021-10-17 22:40:23 +0800

    re-update dependencies

--- laragon/www/cwd-marketing-template/main.py
+++ laragon/www/cwd-marketing-template/main.py
@@ -1,12 +1,12 @@
 # from logging import info
 from flask import Flask, render_template,request, redirect, sessions, url_for, session
 from flask_mysqldb import MySQL     #flask_mysqldb import MySQL
+from flask_mail import Mail,Message
 import MySQLdb 
-
-
 
 #connection with the database.
 #Im using this sql local server
+
 app = Flask(__name__)
 app.secret_key = "canadianwebdesigns!!@"
 app.config["MYSQL_HOST"] = "localhost"
@@ -14,10 +14,17 @@ app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "login"
 
+# parameters required for configuration of email
 
-
+app.config['MAIL_SERVER'] = 'smpt.gmail.com' #server using gmail
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'randolfh.cwd@gmail.com'
+app.config['MAIL_PASSWORD'] = 'randolfhcwd123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 db = MySQL(app)
+mail = Mail(app)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
@@ -33,12 +40,30 @@ def index():
                     return render_template('profile.html')
             else:
                 return render_template('missinginfo.html')
-
-
-
     return render_template('login.html')
 
+#Route for profile page
+@app.route('/profile')
+def index1():
+    return render_template('send_message')
 
+# second route for email server post request
+@app.route('/profile', methods=['GET','POST'])
+def send_message():
+    if request.method == "POST":
+        email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+
+        # Now past to email Library
+        message = Message(subject,sender="randolfh.cwd@gmail.com", recipients=[email])
+        message.body = msg
+
+        # flask sending email method
+        mail.send(message)
+        success = "message has been sent"
+        # After send return to another page
+        return render_template('profile.html', success = success)
 
 if __name__ ==  '__main__':
     app.run(debug=True)
